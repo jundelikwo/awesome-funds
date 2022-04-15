372e2a071b68be5be017f78420b430d324dc1ab6
Jonathan Undelikwo <jundelikwo@gmail.com>
2022-04-15 23:26:57 +0100

    Added EuclidCircularA fonts

---
+++ src/utils/theme.ts
@@ -0,0 +1,10 @@
+// get color names here: http://chir.ag/projects/name-that-color
+export const colors = {};
+
+export const typography = {
+  EUCLID_CIRCULAR_A_BOLD: 'EuclidCircularABold',
+  EUCLID_CIRCULAR_A_LIGHT: 'EuclidCircularALight',
+  EUCLID_CIRCULAR_A_MEDIUM: 'EuclidCircularAMedium',
+  EUCLID_CIRCULAR_A_REGULAR: 'EuclidCircularARegular',
+  EUCLID_CIRCULAR_A_SEMIBOLD: 'EuclidCircularASemiBold',
+};
